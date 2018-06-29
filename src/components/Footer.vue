967d27282faf35cf38f5222009d3530ba80233b8
Geoff Dutton <g.dutton@gmail.com>
2018-06-29 16:46:11 -0500

    add viglink attribution

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,6 +2,7 @@
     <footer class="container">
         <div>Text generated from <a href="http://officeipsum.com/index.php">Office Ipsum</a></div>
         <div>Layout by <a href="https://github.com/philipwalton/solved-by-flexbox">Solved by Flexbox</a></div>
+        <a href="http://www.viglink.com/legal/disclosures/users?vgtag=badge"><img alt="VigLink badge" height="31" src="http://www.viglink.com/images/badges/88x31.png" title="Links monetized by VigLink" width="120" /></a>
     </footer>
 </template>
 
